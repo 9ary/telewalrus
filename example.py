52125d650fb655f4e049a35c7cba44e41056841d
Dan Elkouby <streetwalkermc@gmail.com>
2017-10-03 02:09:36 +0300

    Update example script

--- example.py
+++ example.py
@@ -6,13 +6,13 @@ mybot = bot.Bot("<API token>")
 
 # Global command that works in every chat the bot is in
 @mybot.command("hello")
-async def kick(message):
+async def cmd1(message):
     await message.chat.message("Hello world!")
 
 # Chat-specific command
 mychat = mybot.get_chat(some_chat_id)
 @mychat.command("boop")
-async def lose(message):
+async def cmd2(message):
     await message.chat.message("Boop!")
 
 mybot.run()
